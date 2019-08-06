2a66ba731ef454b620690baaf4fe3bfea1852245
Muhammad Moaz Khan <moazkh60@gmail.com>
2019-08-06 23:10:47 +0500

    Plotting the test set
    Plotting the test set

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -9,7 +9,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:,:-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 1].values
 
 # Splitting the data into training and test sets
 from sklearn.model_selection import train_test_split
@@ -21,9 +21,17 @@ sc_X = StandardScalar()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""
 
+# Fitting simple linear regression
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
+# Predicting results
 y_pred = regressor.predict(X_test)
-a
+
+# Plotting
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlable('Years of Experience')
+plt.ylabel('Salary')
